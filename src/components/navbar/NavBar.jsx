45f2c9475af459296569da4212b71a8fac0b27d6
mahmoud khalid <mahmoud0122549@gmail.com>
2022-12-04 14:17:05 +0200

    complete navbar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,12 +1,32 @@
 import React, { useState } from "react";
 
 import "./navbar.css";
-import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import { RiMenu3Line, RiCloseLine, RiLightbulbFill } from "react-icons/ri";
+import { BsMoonStarsFill } from "react-icons/bs";
 import logo from "../../assets/logo.svg";
 
+const Menu = () => (
+  <>
+    <p>
+      <a href="#home">Home</a>
+    </p>
+    <p>
+      <a href="#gpt3">What is GPT3?</a>
+    </p>
+    <p>
+      <a href="#ai">Open AI</a>
+    </p>
+    <p>
+      <a href="#feature">Case Studies</a>
+    </p>
+    <p>
+      <a href="#blog">Library</a>
+    </p>
+  </>
+);
 const NavBar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
-
+  const [toggleMode, setToggleMode] = useState(true);
   return (
     <div className="gpt">
       <div className="links">
@@ -14,21 +34,7 @@ const NavBar = () => {
           <img src={logo} alt="logo" />
         </div>
         <div className="container">
-          <p>
-            <a href="#home">Home</a>
-          </p>
-          <p>
-            <a href="#gpt3">What is GPT3?</a>
-          </p>
-          <p>
-            <a href="#ai">Open AI</a>
-          </p>
-          <p>
-            <a href="#feature">Case Studies</a>
-          </p>
-          <p>
-            <a href="#blog">Library</a>
-          </p>
+          <Menu />
         </div>
       </div>
       <div className="sign">
@@ -39,20 +45,53 @@ const NavBar = () => {
         {toggleMenu ? (
           <RiCloseLine
             color="#fff"
+            transition="1s"
             size={27}
             onClick={() => setToggleMenu(false)}
           />
         ) : (
           <RiMenu3Line
             color="#fff"
+            transition="1s"
             size={27}
             onClick={() => {
               setToggleMenu(true);
             }}
           />
         )}
+        {toggleMenu && (
+          <div className="menu-container scale-up-center">
+            <div className="menu_container_links">
+              <Menu />
+              <div className="menu-sign">
+                <p>Sign in</p>
+                <button type="button">Sign up</button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
-      <div className="mode"></div>
+      <div className="mode">
+        {toggleMode ? (
+          <RiLightbulbFill
+            color="#fff"
+            transition="1s"
+            size={27}
+            onClick={() => {
+              setToggleMode(false);
+            }}
+          />
+        ) : (
+          <BsMoonStarsFill
+            color="#fff"
+            transition="1s"
+            size={27}
+            onClick={() => {
+              setToggleMode(true);
+            }}
+          />
+        )}
+      </div>
     </div>
   );
 };
