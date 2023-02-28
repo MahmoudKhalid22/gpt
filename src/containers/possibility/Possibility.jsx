7936062ebf96c44282c44f9d7692f2a320e376cf
mahmoud khalid <mahmoud0122549@gmail.com>
2023-03-01 00:14:26 +0200

    production

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,25 +1,27 @@
 import React from "react";
-import possibilityImage from "../../assets/possibility.png";
 import "./possibility.css";
+import possibilityImage from "../../assets/possibility.png";
 
 function Possibility() {
   return (
-    <div className="poss section-padding" id="possibility">
-      <div className="possibility-img">
-        <img src={possibilityImage} alt="possibility" />
-      </div>
-      <div className="possibility_content">
-        <h4 className="one">Request Early Access to Get Started</h4>
-        <h1 className="gradint-text">
-          The possibilities are beyond your imagination
-        </h1>
-        <p className="first">
-          Yet bed any for travelling assistance indulgence unpleasing. Not
-          thoughts all exercise blessing. Indulgence way everything joy
-          alteration boisterous the attachment. Party we years to order allow
-          asked of.
-        </p>
-        <h4 className="second">Request Early Access to Get Started</h4>
+    <div className="section-padding" id="possibility">
+      <div className="poss">
+        <div className="possibility-img">
+          <img src={possibilityImage} alt="possibility" />
+        </div>
+        <div className="possibility_content">
+          <h4 className="one">Request Early Access to Get Started</h4>
+          <h1 className="gradint-text">
+            The possibilities are beyond your imagination
+          </h1>
+          <p className="first">
+            Yet bed any for travelling assistance indulgence unpleasing. Not
+            thoughts all exercise blessing. Indulgence way everything joy
+            alteration boisterous the attachment. Party we years to order allow
+            asked of.
+          </p>
+          <h4 className="second">Request Early Access to Get Started</h4>
+        </div>
       </div>
     </div>
   );
